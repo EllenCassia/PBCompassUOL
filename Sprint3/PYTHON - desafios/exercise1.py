20845a2882fcdfc82ca0d88e99b7fe97e8c38bae
Ellen Cassia <ellencassiamatos@gmail.com>
2023-08-31 20:09:58 -0300

    :books: docs: Uptade README.md

--- Sprint3/PYTHON - desafios/exercise1.py
+++ Sprint3/PYTHON - desafios/exercise1.py
@@ -1,1 +1,14 @@
-2 / 2
+#Desenvolva um código Python que lê do teclado nome e a idade atual de uma pessoa. Como saída, imprima o ano em que a pessoa completará 100 anos de idade.
+
+nome = input("Nome: ")
+idade = int(input("Idade: "))
+
+
+# Obtém o ano atual
+from datetime import datetime
+anoAtual = datetime.now().year
+
+idadeCem = 100 - idade
+anoCompletaCem = anoAtual + idadeCem
+print(anoCompletaCem)
+
