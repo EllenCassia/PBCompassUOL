5ee21418e6236c0cc5d80c69a44e61c16bcf0425
EllenCassia <102759780+EllenCassia@users.noreply.github.com>
2023-09-13 10:43:20 -0300

    :sparkles: create new file

--- Sprint3/PYTHON - desafios/exercicio19.py
+++ Sprint3/PYTHON - desafios/exercicio19.py
@@ -1,25 +1,22 @@
 import random
 
-random_list = [random.sample(range(500), 50)]
+random_list = random.sample(range(500),50)
 
 
 mediana = 0
 media = 0
 valor_minimo = 0
 valor_maximo = 0
- 
 
-for i in range(len(random_list)):
-    if len(random_list) % 2 == 0:
-        mediana = (random_list[len(random_list) // 2] + random_list[len(random_list) // 2 - 1]) // 2
-    else:
-        mediana = random_list[len(random_list) // 2]
+random_list.sort()
+ 
+if len(random_list) % 2 == 0:
+    mediana = float((random_list[len(random_list) // 2] + random_list[(len(random_list) // 2) - 1])) // 2   
+else:
+    mediana = random_list[len(random_list) // 2]
 
 media = sum(random_list) / len(random_list)
 valor_maximo = max(random_list)
 valor_minimo = min(random_list)
 
-print(media)
-print(valor_maximo)
-print(valor_minimo)
-print(mediana)
+print(f'Media: {media}, Mediana: {mediana}, Mínimo: {valor_minimo}, Máximo: {valor_maximo}')
